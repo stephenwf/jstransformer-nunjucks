2a6895836fed506f497f1e8cbd95728fb80116f8
stephenwf <stephen.fraser@digirati.com>
2017-12-12 22:30:59 +0000

    Twig/Jinja compat + node resolving

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 'use strict'
 
 const path = require('path')
+const fs = require('fs')
 const nunjucks = require('nunjucks')
 const extend = require('extend-shallow')
 
-exports.name = 'nunjucks'
-exports.inputFormats = ['njk', 'nunjucks']
+exports.name = 'nunjucks-compat'
+exports.inputFormats = ['njk', 'nunjucks', 'twig']
 exports.outputFormat = 'html'
+
+nunjucks.installJinjaCompat()
+
+const NodeLoader = nunjucks.FileSystemLoader.extend({
+  getSource(name) {
+    let fullpath
+    try {
+      fullpath = require.resolve(name)
+    } catch (err) {
+      return nunjucks.FileSystemLoader.prototype.getSource.call(this, name)
+    }
+
+    return {
+      src: fs.readFileSync(fullpath, 'utf-8'),
+      path: fullpath,
+      noCache: this.noCache
+    }
+  }
+})
 
 exports.compile = function (str, options) {
   // Prepare the options.
@@ -14,12 +34,18 @@ exports.compile = function (str, options) {
 
   // Find the path for which the environment will be created.
   const envpath = opts.root || opts.path || (opts.filename ? path.dirname(opts.filename) : null)
-  let env = null
+  const loaders = [new NodeLoader()]
+  const paths = [process.cwd()]
+
   if (envpath) {
-    env = nunjucks.configure(envpath, opts)
-  } else {
-    env = nunjucks.configure(opts)
+    paths.push(envpath)
   }
+
+  loaders.push(new nunjucks.FileSystemLoader(paths))
+
+  const env = new nunjucks.Environment(loaders, opts)
+
+  env.addFilter('raw', env.getFilter('safe'))
 
   // Add all the Filters.
   for (const name in opts.filters || {}) {
