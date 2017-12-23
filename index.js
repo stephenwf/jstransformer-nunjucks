3fa0bb6c909d243f878c1d3a5847a11b1167ac42
stephenwf <stephen.fraser@digirati.com>
2017-12-23 02:23:19 +0000

    Ignores front matter notation when including

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ exports.outputFormat = 'html'
 
 nunjucks.installJinjaCompat()
 
+// https://github.com/jxson/front-matter/blob/master/index.js#L2-L13
+const optionalByteOrderMark = '\\ufeff?'
+const pattern = '^(' +
+  optionalByteOrderMark +
+  '(= yaml =|---)' +
+  '$([\\s\\S]*?)' +
+  '^(?:\\2|\\.\\.\\.)' +
+  '$' +
+  (process.platform === 'win32' ? '\\r?' : '') +
+  '(?:\\n)?)'
+// NOTE: If this pattern uses the 'g' flag the `regex` variable definition will
+// need to be moved down into the functions that use it.
+const regex = new RegExp(pattern, 'm')
+
+function ignoreFrontMatter(file) {
+  return file.replace(regex, '')
+}
+
 const NodeLoader = nunjucks.FileSystemLoader.extend({
   getSource(name) {
     let fullpath = null
@@ -36,7 +54,7 @@ const NodeLoader = nunjucks.FileSystemLoader.extend({
     this.pathsToNames[fullpath] = name
 
     return {
-      src: fs.readFileSync(fullpath, 'utf-8'),
+      src: ignoreFrontMatter(fs.readFileSync(fullpath, 'utf-8')),
       path: fullpath,
       noCache: this.noCache
     }
